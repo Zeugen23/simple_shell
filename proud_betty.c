e70ae7d10e92392d1f646fb1b76bf5c781d06221
Zeugen23 <ulrichparvel@gmail.com>
2023-11-15 06:09:35 -0800

    T

--- proud_betty.c
+++ proud_betty.c
@@ -6,11 +6,9 @@ int main(void)
     int a = 2;
     int b = 6;
     int difference = b - a;
-    pid_t pid;
-    pid = getpid();
 
     printf("Difference is %d\n", difference);
-    printf("PID is %d\n", pid);
+
 
     return 0;
 }
