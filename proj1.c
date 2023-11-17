872147c3f7c3181f30d92f5695720d4e45b11f6c
Zeugen23 <ulrichparvel@gmail.com>
2023-11-17 04:18:46 -0800

    second commit

--- proj1.c
+++ proj1.c
@@ -5,35 +5,42 @@
 
 #define BUFFER_SIZE 1024
 
-int main(void) {
-    char command[BUFFER_SIZE];
-    int exit_flag = 0;
+int main(void)
+{
+	char command[BUFFER_SIZE];
+	int exit_flag = 0;
 
-    while (!exit_flag) {
-        printf("$ "); /* Display the prompt */
-        fflush(stdout); /* Ensure the prompt is displayed */
+	while (!exit_flag)
+	{
+		printf("$\n"); /* Display the prompt */
+		fflush(stdout); /* Ensure the prompt is displayed */
 
-        if (fgets(command, BUFFER_SIZE, stdin) == NULL) {
-            /* Handle end of file (Ctrl+D) */
-            printf("\n");
-            break;
-        }
+		if (fgets(command, BUFFER_SIZE, stdin) == NULL)
+		{
+			/* Handle end of file (Ctrl+D) */
+			printf("\n");
+			break;
+		}
 
-        /* Remove the newline character from the input */
-        command[strcspn(command, "\n")] = '\0';
+		/* Remove the newline character from the input */
+		command[strcspn(command, "\n")] = '\0';
 
-        if (strcmp(command, "exit") == 0) {
-            /* Exit the shell if the user inputs "exit" */
-            exit_flag = 1;
-        } else {
-            /* Execute the command */
-            int status = system(command);
+		if (strcmp(command, "exit") == 0)
+		{
+			/* Exit the shell if the user inputs "exit" */
+			exit_flag = 1;
+		}
+		else
+		{
+			/* Execute the command */
+			int status = system(command);
 
-            if (status == -1) {
-                printf("Error executing command\n");
-            }
-        }
-    }
+			if (status == -1)
+			{
+				printf("Error executing command\n");
+			}
+		}
+	}
 
-    return 0;
+	return (0);
 }
