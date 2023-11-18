c1d4d6847f4c043dbcae6e1117f8ed5b7fe0f85e
Zeugen23 <ulrichparvel@gmail.com>
2023-11-17 17:22:54 -0800

    seventh commit

--- project3.c
+++ project3.c
@@ -7,15 +7,18 @@
 #define BUFFER_SIZE 1024
 #define ARGUMENTS_SIZE 64
 
-char* get_command_path(char* command) {
-    char* path = getenv("PATH");
-    char* token = strtok(path, ":");
+char *get_command_path(char *command)
+{
+    char *path = getenv("PATH");
+    char *token = strtok(path, ":");
 
-    while (token != NULL) {
-        char* command_path = malloc(strlen(token) + strlen(command) + 2);
-        sprintf(command_path, "%s/%s", token, command);
+    while (token != NULL)
+    {
+        char *command_path = malloc(strlen(token) + strlen(command) + 2);
+        snprintf(command_path, strlen(token) + strlen(command) + 2, "%s/%s", token, command);
 
-        if (access(command_path, F_OK) == 0) {
+        if (access(command_path, F_OK) == 0)
+        {
             return command_path;
         }
 
@@ -27,63 +30,74 @@ char* get_command_path(char* command) {
     return NULL;
 }
 
-int project3_main(void) {
+int project3_main(void)
+{
     char buffer[BUFFER_SIZE];
     int status;
     pid_t pid;
-    char* command;
-    char* arguments[ARGUMENTS_SIZE];
+    char *command;
+    char *arguments[ARGUMENTS_SIZE];
     int argument_count;
-    char* command_path; /* Declaration moved to the top */
+    char *command_path;
 
-    while (1) {
+    while (1)
+    {
         printf("$ ");
         fflush(stdout);
 
-        if (fgets(buffer, BUFFER_SIZE, stdin) == NULL) {
+        if (fgets(buffer, BUFFER_SIZE, stdin) == NULL)
+        {
             printf("\n");
             break;
         }
 
-        buffer[strcspn(buffer, "\n")] = '\0'; /* Remove trailing newline character */
+        buffer[strcspn(buffer, "\n")] = '\0';
 
-        command = strtok(buffer, " "); /* Split command line by space delimiter */
+        command = strtok(buffer, " ");
 
         argument_count = 0;
 
-        while (command != NULL) {
+        while (command != NULL)
+        {
             arguments[argument_count] = command;
             argument_count++;
             command = strtok(NULL, " ");
         }
 
-        arguments[argument_count] = NULL; /* Set the last element to NULL as required by exec family functions */
+        arguments[argument_count] = NULL;
 
-        if (argument_count == 0) {
-            continue; /* Empty command, prompt again */
+        if (argument_count == 0)
+        {
+            continue;
         }
 
         command_path = get_command_path(arguments[0]);
 
-        if (command_path == NULL) {
+        if (command_path == NULL)
+        {
             printf("Command not found: %s\n", arguments[0]);
             continue;
         }
 
         pid = fork();
 
-        if (pid == -1) {
+        if (pid == -1)
+        {
             perror("fork");
             exit(EXIT_FAILURE);
         }
 
-        if (pid == 0) {
+        if (pid == 0)
+        {
             /* Child process */
-            if (execv(command_path, arguments) == -1) {
+            if (execv(command_path, arguments) == -1)
+            {
                 perror("execv");
                 exit(EXIT_FAILURE);
             }
-        } else {
+        }
+        else
+        {
             /* Parent process */
             waitpid(pid, &status, 0);
         }
@@ -94,6 +108,7 @@ int project3_main(void) {
     return 0;
 }
 
-int main_function(void) {
+int my_main(void)
+{
     return project3_main();
 }
