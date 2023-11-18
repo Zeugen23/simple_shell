71155edf0120cca6bcc05b5e838f62e10d05c4b6
Zeugen23 <ulrichparvel@gmail.com>
2023-11-17 18:03:18 -0800

    script1 shell

---
+++ script1.sh
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * print_message - Prints a message to the console.
+ */
+void print_message(void)
+{
+	printf("Hello, world!\n");
+}
+
+/**
+ * execute_program - Entry point of the program.
+ *
+ * Return: Always 0.
+ */
+int execute_program(void)
+{
+	print_message();
+	return (0);
+}
